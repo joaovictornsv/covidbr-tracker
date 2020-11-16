c73b835c5d9c60521e22d84fc60dd7afef0bebdd
joaovictornsv <joaovictornsv@gmail.com>
2020-11-16 16:31:21 -0300

    refactor: flags source

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -21,43 +21,6 @@ export interface StatsProps {
   datetime?: string
 }
 
-const bandeira = require.context( '../assets/images/bandeiras', true, /\.(png|jpe?g|svg)$/);
-
-const paths = bandeira.keys ()
-
-const images = paths.map( path => bandeira ( path ) )
-
-const bandeirasPNG = {
-  AC: images[0],
-  AL: images[1],
-  AM: images[2],
-  AP: images[3],
-  BA: images[4],
-  CE: images[5],
-  DF: images[6],
-  ES: images[7],
-  GO: images[8],
-  MA: images[9],
-  MG: images[10],
-  MS: images[11],
-  MT: images[12],
-  PA: images[13],
-  PB: images[14],
-  PE: images[15],
-  PI: images[16],
-  PR: images[17],
-  RJ: images[18],
-  RN: images[19],
-  RO: images[20],
-  RR: images[21],
-  RS: images[22],
-  SC: images[23],
-  SE: images[24],
-  SP: images[25],
-  TO: images[26],
-  BR: images[27],
-}
-
 const Stats: React.FC<StatsProps> = ( { state, cases, deaths, suspects, refuses, uf}) => {
 
   let letality = 0
@@ -66,22 +29,13 @@ const Stats: React.FC<StatsProps> = ( { state, cases, deaths, suspects, refuses,
     letality = (deaths / cases * 100)
   }
 
-  let flagPath = ''
-
-  Object.entries(bandeirasPNG).map(key => {
-  if (key[0] === uf) {
-    flagPath = key[1]
-  }
-  return 0;
-  })
-  
   return (
     <div className="stats-wrapper">
       <div id="stats-box">
         <div className="stats-content">
           <div className="stats-header">
             <div className="local">
-              <img className="local-flag"  src={flagPath} alt={`Bandeira do ${state}`}/>
+              <img className="local-flag"  src={`https://raw.githubusercontent.com/bgeneto/bandeiras-br/master/imagens/${uf}.png`} alt={`Bandeira do ${state}`}/>
               <h1>{state}</h1>
             </div>    
             <div className="back-icon">   
